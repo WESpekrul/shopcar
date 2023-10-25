ec46181cf6f875391aeb7a30e5352d30d1d2c892
WEsley Pekrul <wes.pekrul.oli@gmail.com>
2023-10-25 19:15:56 -0300

    Terc

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./navbar.css";
+import {FaInstagram, FaWhatsapp, FaYoutube, FaFacebook} from "react-icons/fa"
+
+const Navbar = () => {
+    return (
+        <div className="nvb-container">
+            <div className="nvb-content">
+                <h1>ShopCar</h1>
+                <ul> 
+                    <li>Home</li>
+                    <li>Estoque</li>
+                    <li>Vendas</li>
+                    <li>Compra</li>
+                    <li>Contatos</li>
+                </ul>
+
+                <div className="nvb-contato">
+                    <FaWhatsapp />
+                    <span>11 23181710</span>
+                </div>
+
+                <div className="nvb-socialmedia"> 
+                    <FaInstagram />
+
+                    <FaYoutube />
+
+                    <FaFacebook />
+                
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
