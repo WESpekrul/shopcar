431134c645f3ffa71a1e14c8c9f62121f0143edc
WESpekrul <wes.pekrul.oli@gmail.com>
2023-11-24 10:54:49 -0300

    24NovEstoque

--- src/pages/Estoque/estoque.jsx
+++ src/pages/Estoque/estoque.jsx
@@ -2,6 +2,8 @@ import "./estoque.css";
 import Navbar from "../../components/navbar/Navbar"
 import React from "react";
 import Footer from "../../components/footer/Footer";
+import { BiSearch } from "react-icons/bi"
+import "./estoque.css"
 
 const Estoque = () => {
     return (
@@ -12,13 +14,13 @@ const Estoque = () => {
             <div className ="est-content">
             <div className ="est-banner">
                 <h1>Estoque</h1>
-                <h2>Carros selecionados pelo nosso time para garantir
-
-
+                <h2>Carros selecionados pelo nosso time para garantir <br></br>
+                uma otíma compra do seu usado 
                 </h2>
-
-
-
+                <div className = "est-search-content">
+                    <input type="text" id="txtSearch"/>
+                    <BiSearch />
+                </div>
             </div>
             </div>
             </div>
